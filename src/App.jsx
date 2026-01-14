e66113508c18e9c0c6aa9f6c6f6e682ad6d5c6d4
Your Name <your-email@example.com>
2026-01-14 17:02:40 +0530

    Structure added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-blue-950 text-4xl'>Test</h1>
+    </>
+  )
+}
+
+export default App
