cfc22c56ae491b6e6cd0d6b2e713093c5cd61b61
PAYAL KHODE <khodepayal2005@gmail.com>
2026-01-14 17:35:17 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-blue-950 text-4xl'> Payal Test  3 </h1>
-      
+      <h1 className='text-blue-950 text-4xl'>Test</h1>
     </>
   )
 }
